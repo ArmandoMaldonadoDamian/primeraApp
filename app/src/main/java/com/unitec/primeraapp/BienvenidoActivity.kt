0ef66f72089e3709f3c92cf1929fa55089d4b058
SE <arman.28md@hotmail.com>
2019-10-01 19:36:28 -0500

    clase lunes sonidos

--- app/src/main/java/com/unitec/primeraapp/BienvenidoActivity.kt
+++ app/src/main/java/com/unitec/primeraapp/BienvenidoActivity.kt
@@ -1,10 +1,12 @@
 package com.unitec.primeraapp
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 
 import kotlinx.android.synthetic.main.activity_bienvenido.*
+import kotlinx.android.synthetic.main.content_bienvenido.*
 
 class BienvenidoActivity : AppCompatActivity() {
 
@@ -14,7 +16,14 @@ class BienvenidoActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            var sonido=MediaPlayer.create(applicationContext,R.raw.sad)
+            //SE INICIA EL SONIDO
+            sonido.start()
+            var peso=txtPeso.text.toString().toFloat()
+            var altura=txtAltura.text.toString().toFloat()
+            var imc=MiImc()
+
+            Snackbar.make(view, "Tu imc de tu peso es $peso es"+" ${imc.calcular(peso,altura)}y ya", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
--- app/src/main/java/com/unitec/primeraapp/MiImc.kt
+++ app/src/main/java/com/unitec/primeraapp/MiImc.kt
@@ -2,7 +2,7 @@ package com.unitec.primeraapp
 
 class MiImc {
     //vamos a generar el metodo para calcular el IMC
-    fun calcular(peso:float, altura:float):float{
+    fun calcular(peso:Float, altura:Float):Float{
        var imc=peso/(altura*altura)
         return imc
     }
